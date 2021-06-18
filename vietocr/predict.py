738eaa22143e8694d85934d1e62ae7ba8e743ee5
anhtv26062000 <anhtv26062000@gmai.com>
2021-06-18 09:45:18 +0000

    edit file to predict folder img and save file result txt

--- vietocr/predict.py
+++ vietocr/predict.py
@@ -1,23 +1,59 @@
+import os
+import time
 import argparse
 from PIL import Image
+import matplotlib.pyplot as plt
 
 from vietocr.tool.predictor import Predictor
 from vietocr.tool.config import Cfg
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--img', required=True, help='foo help')
-    parser.add_argument('--config', required=True, help='foo help')
+    parser.add_argument("--img", required=True, help="foo help")
+    parser.add_argument("--config", required=True, help="foo help")
 
     args = parser.parse_args()
     config = Cfg.load_config_from_file(args.config)
 
     detector = Predictor(config)
 
-    img = Image.open(args.img)
-    s = detector.predict(img)
+    # Option for predicting folder images
+    img_list = os.listdir(args.img)
+    img_list = sorted(img_list)
 
-    print(s)
+    f_pre = open("./test_seq.txt", "w+")
 
-if __name__ == '__main__':
+    # new output <name>\t<gtruth>\t<predict>
+    # f_gt = open("./gt_word.txt", "r")
+    # lines = [line.strip("\n") for line in f_gt if line != "\n"]
+
+    # start_time = time.time()
+    # for img in lines:
+    #     name, gt = img.split("\t")
+    #     img_path = args.img + name
+    #     image = Image.open(img_path)
+
+    #     s, prob = detector.predict(image, return_prob=True)
+
+    #     res = name + "\t" + gt + "\t" + s + "\t" + str(prob) + "\n"
+    #     f_pre.write(res)
+    # runtime = time.time() - start_time
+    # print("FPS:", len(img_list) / runtime)
+
+    start_time = time.time()
+    for img in img_list:
+        img_path = args.img + img
+        image = Image.open(img_path)
+
+        s = detector.predict(image)
+        # print(img_path, "-----", s)
+
+        res = img + "\t" + s + "\n"
+        f_pre.write(res)
+    runtime = time.time() - start_time
+    print("FPS:", len(img_list) / runtime)
+
+
+if __name__ == "__main__":
     main()
